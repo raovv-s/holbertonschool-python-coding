ca31f919037102eaffa7c8880fe572b8f013cf9f
raovv-s <nadirmaharramov875@gmail.com>
2025-11-23 14:28:52 +0000

    sdjks

---
+++ python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ using raise in init """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set size with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+    
+    def my_print(self):
+        s = ""
+        if size == 0:
+            print()
+        for i in range(self.size):
+            s += '#'
+            return s
