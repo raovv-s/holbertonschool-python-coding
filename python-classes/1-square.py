796abbb751fb8f0e0c0695ad9fa02784f5621aad
raovv-s <nadirmaharramov875@gmail.com>
2025-11-23 13:42:59 +0000

    sdjk

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,10 +2,10 @@
 """ Square Module """
 
 class Square:
+    """ using raise in init """
     def __init__(self, size=0):
         self.__size = size
         if size < 0:
             raise ValueError("size must be >= 0")
         if not isinstance(int, size):
             raise TypeError("size must be an integer")
-
