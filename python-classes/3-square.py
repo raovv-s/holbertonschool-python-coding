c6d7ef0c6313dd2e6f52f03ad012975e1191d29b
raovv-s <nadirmaharramov875@gmail.com>
2025-11-23 14:10:09 +0000

    dsdsl

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -15,6 +15,6 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-
+        self.__size = value    
     def area(self):
         return self.__size * self.__size
