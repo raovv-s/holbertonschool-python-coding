b460a7188d962f1f16ab31d9a7a72e70658642bd
raovv-s <nadirmaharramov875@gmail.com>
2025-11-23 14:08:46 +0000

    sdkdsl

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,10 +6,6 @@ class Square:
     """ using raise in init """
     def __init__(self, size=0):
         self.__size = size
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
 
     def size(self):
         return self.__size
