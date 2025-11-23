120995970f49dca5a87e23a18d79ee3a61dcf20f
raovv-s <nadirmaharramov875@gmail.com>
2025-11-23 14:07:58 +0000

    sjdk

---
+++ python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ using raise in init """
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def size(self):
+        return self.__size
+
+    def size(self, size):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
