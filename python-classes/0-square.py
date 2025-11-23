10cf10811428928cc2862c318118d9612ea9bb5b
raovv-s <nadirmaharramov875@gmail.com>
2025-11-23 13:31:28 +0000

    sdjs

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ Square Module"""
 
+
 class Square:
-    """ init function for object """
+    """ init function for object , size is private attribute """
     def __init__(self, size):
         self.__size = size
