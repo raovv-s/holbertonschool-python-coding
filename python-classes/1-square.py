42a3aa1e42cff7f868a17fba093df9d2554aeade
raovv-s <nadirmaharramov875@gmail.com>
2025-11-23 13:43:52 +0000

    sdls

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,5 +7,5 @@ class Square:
         self.__size = size
         if size < 0:
             raise ValueError("size must be >= 0")
-        if not isinstance(int, size):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
